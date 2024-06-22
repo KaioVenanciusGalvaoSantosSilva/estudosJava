e7774abed40f2e3c78fbc7371b1cfbd7af7fcf31
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-22 19:24:00 -0300

    anatomia das classes

---
+++ java-anatomia-classes/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,6 @@
+public class MinhaClasse {
+    public static void main(String[] args) {//classse principal do projeto 
+        System.out.print("Olá mundo."); // Classe Java System -> realizará saida no console
+    }
+
+}
