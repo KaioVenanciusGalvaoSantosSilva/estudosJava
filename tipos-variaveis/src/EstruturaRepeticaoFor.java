7550ba22f5b3c313248d00caea27f6b1a380dd8b
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-23 12:37:36 -0300

    Estrutura de repetição for

---
+++ tipos-variaveis/src/EstruturaRepeticaoFor.java
@@ -0,0 +1,15 @@
+public class EstruturaRepeticaoFor {
+    public static void main(String[] args) {
+        //Estrutura de repetição for
+
+        //for (inicialização; condição; iteração) {
+            // Bloco de código a ser repetido enquanto a condição for verdadeira
+        //}
+        //Exemplo:
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Contagem: " + i);
+        }
+
+    }
+}
