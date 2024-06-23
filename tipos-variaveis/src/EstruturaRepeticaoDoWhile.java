b1e461ec724cad0ee411f3f61c1b1a906536f532
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-23 12:45:32 -0300

    Estrutura de repetição Do While

---
+++ tipos-variaveis/src/EstruturaRepeticaoDoWhile.java
@@ -0,0 +1,16 @@
+public class EstruturaRepeticaoDoWhile {
+    public static void main(String[] args) {
+        //Estrutura de repetição do while
+
+        //do {
+            // Bloco de código a ser repetido pelo menos uma vez
+        //} while (condição);
+
+        //Exemplo:
+        int contador = 1;
+        do {
+            System.out.println("Contagem: " + contador);
+            contador++;
+        } while (contador <= 5);   
+    }
+}
