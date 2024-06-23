3e2cbfbec9b8f14ef8fdf432da2528922e4f0225
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-23 12:39:12 -0300

    Estrutura de Repeticao ForEach

---
+++ tipos-variaveis/src/EstruturaRepeticaoForEach.java
@@ -0,0 +1,17 @@
+public class EstruturaRepeticaoForEach {
+
+    public static void main(String[] args) {
+    //Estrutura de repetição for each
+
+        //for (tipo variável : coleção) {
+            // Bloco de código a ser repetido para cada elemento da coleção
+        //}
+
+        //Exemplo:
+            String[] nomes = {"João", "Maria", "Pedro"};
+        for (String nome : nomes) {
+            System.out.println("Olá, " + nome);
+        }
+    }
+
+}
