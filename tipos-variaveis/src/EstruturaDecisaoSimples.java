8af795673db597393b04fb3425c10532817fb58f
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-23 12:16:45 -0300

    Estrutura de decisão simples

---
+++ tipos-variaveis/src/EstruturaDecisaoSimples.java
@@ -0,0 +1,15 @@
+public class EstruturaDecisaoSimples {
+    public static void main(String[] args) {
+        // Estrutura de decisão simples
+
+        //if (condição) {
+            // Bloco de código a ser executado se a condição for verdadeira
+        //}
+
+        // Exemplo:
+        int idade = 18;
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade.");
+        }
+    }
+}
