1caa31874a4b8b8d33023d864ff8272bd6f0b66c
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-23 12:31:42 -0300

    Estrutura de decisão encadeada

---
+++ tipos-variaveis/src/EstruturaDecisaoEncadeada.java
@@ -0,0 +1,28 @@
+public class EstruturaDecisaoEncadeada {
+
+    public static void main(String[] args) {
+        //Estrutura de decisão encadeada
+
+        //if (condição1) {
+            // Bloco de código a ser executado se a condição1 for verdadeira
+        //} else if (condição2) {
+            // Bloco de código a ser executado se a condição2 for verdadeira
+        //} else {
+            // Bloco de código a ser executado se todas as condições acima forem falsas
+        //}
+
+        //Exemplo:
+
+        int nota = 85;
+        if (nota >= 90) {
+            System.out.println("Nota A");
+        } else if (nota >= 80) {
+            System.out.println("Nota B");
+        } else if (nota >= 70) {
+            System.out.println("Nota C");
+        } else {
+            System.out.println("Nota D");
+        }
+    }
+
+}
