e15e5db88f80a84584aa92e1a979b94a5be255c2
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-23 12:20:06 -0300

    Estrutura de decisão composta

---
+++ tipos-variaveis/src/EstruturaDecisaoComposta.java
@@ -0,0 +1,22 @@
+public class EstruturaDecisaoComposta {
+    public static void main(String[] args) {
+        
+        //Estrutura de decisão composta
+
+        //if (condição) {
+            // Bloco de código a ser executado se a condição for verdadeira
+        //} else {
+            // Bloco de código a ser executado se a condição for falsa
+        //}
+
+        //Exemplo:
+        int idade = 16;
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade.");
+        } else {
+            System.out.println("Você é menor de idade.");
+        }
+
+    }
+
+}
