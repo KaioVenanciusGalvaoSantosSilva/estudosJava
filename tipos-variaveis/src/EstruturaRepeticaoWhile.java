f914daf62d9a0fe2d3bfd6f1e42545e43f1f9601
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-23 12:42:53 -0300

    Estrutura de repetição while

---
+++ tipos-variaveis/src/EstruturaRepeticaoWhile.java
@@ -0,0 +1,16 @@
+public class EstruturaRepeticaoWhile {
+    public static void main(String[] args) {
+        //Estrutura de repetição while
+
+        //while (condição) {
+            // Bloco de código a ser repetido enquanto a condição for verdadeira
+        //}
+        //Exemplo:
+
+            int contador = 1;
+        while (contador <= 5) {
+            System.out.println("Contagem: " + contador);
+            contador++;
+        }        
+    }
+}
