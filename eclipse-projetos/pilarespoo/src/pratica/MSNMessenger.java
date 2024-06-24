f24089380d2641aa7a82aede252e0c8647c5af87
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-24 08:24:25 -0300

    Pratica POO

--- eclipse-projetos/pilarespoo/src/pratica/MSNMessenger.java
+++ eclipse-projetos/pilarespoo/src/pratica/MSNMessenger.java
@@ -3,15 +3,14 @@ package pratica;
 public class MSNMessenger {
 	public void enviarMensagem() {
 		System.out.println("Enviando mensagem");
-		salvarHistoricoMensagem();
 	}
 	public void receberMensagem() {
 		System.out.println("Recebendo mensagem");
 	}
-	private void validarConectadoInternet() {
+	public void validarConectadoInternet() {
 		System.out.println("Validando se está conectado a internet");
 	}
-	private void salvarHistoricoMensagem() {
+	public void salvarHistoricoMensagem() {
 		System.out.println("Salvando o histórico da mensagem");
 	}
 }
