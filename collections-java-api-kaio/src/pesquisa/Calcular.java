9363a330621a69521dc5842fd531557009aa56d9
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-06-26 16:14:09 -0300

    Desafio Soma de Números - Pesquisa em List - Collection Framework API Java

---
+++ collections-java-api-kaio/src/pesquisa/Calcular.java
@@ -0,0 +1,20 @@
+package pesquisa;
+
+public class Calcular {
+    public static void main(String[] args) {
+
+        SomaNumeros somaNumeros = new SomaNumeros();
+
+        somaNumeros.adicionarNumero(6);
+        somaNumeros.adicionarNumero(5);
+        somaNumeros.adicionarNumero(7);
+        somaNumeros.adicionarNumero(2);
+        somaNumeros.adicionarNumero(-10);
+        somaNumeros.adicionarNumero(19);
+
+        System.out.println("A soma número da soma é: " + somaNumeros.calcularSoma());
+        System.out.println("O maior número da soma é: " + somaNumeros.encontrarMaiorNumero());
+        System.out.println("O menor número da soma é: " + somaNumeros.encontrarMenorNumero());
+        System.out.println("A lista é composta por: " + somaNumeros.exibirNumeros());
+    }
+}
---
+++ collections-java-api-kaio/src/pesquisa/SomaNumeros.java
@@ -0,0 +1,72 @@
+package pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomaNumeros {
+
+    private final List<Integer> numerosList;
+
+    public SomaNumeros() {
+        this.numerosList = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+        this.numerosList.add(numero);
+    }
+
+    public int calcularSoma() {
+        int soma = 0;
+
+        if (!numerosList.isEmpty()) {
+            for (Integer numero : numerosList) {
+                soma = soma + numero;
+            }
+        } else {
+            return 0;
+        }
+
+        return soma;
+    }
+
+    public int encontrarMaiorNumero() {
+        int maiorNumero = 0;
+
+        if (!numerosList.isEmpty()) {
+            maiorNumero = numerosList.getFirst();
+
+            for (Integer numero : numerosList) {
+                if (numero >= maiorNumero) {
+                    maiorNumero = numero;
+                }
+
+            }
+        }
+        return maiorNumero;
+    }
+
+    public int encontrarMenorNumero() {
+        int menorNumero = 0;
+
+        if (!numerosList.isEmpty()) {
+            menorNumero = numerosList.getFirst();
+
+            for (Integer numero : numerosList) {
+
+                if (numero <= menorNumero) {
+                    menorNumero = numero;
+                }
+            }
+        }
+        return menorNumero;
+    }
+
+    public List<Integer> exibirNumeros() {
+
+        if (numerosList.isEmpty()) {
+            System.out.println("A lista está vazia!");
+        }
+        return numerosList;
+    }
+
+}
