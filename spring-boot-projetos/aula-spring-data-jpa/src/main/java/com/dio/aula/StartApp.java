200434128708312e16718e56ce80d1eb635ce422
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-08-16 23:02:46 -0300

    DATA-JPA

---
+++ spring-boot-projetos/aula-spring-data-jpa/src/main/java/com/dio/aula/StartApp.java
@@ -0,0 +1,30 @@
+package com.dio.aula;
+
+import com.dio.aula.model.User;
+import com.dio.aula.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartApp implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User user = new User();
+        user.setName("KAIO");
+        user.setUsername("kah");
+        user.setPassword("123456");
+
+        repository.save(user);
+
+        for (User u: repository.findAll()){
+            System.out.println(u);
+        }
+
+    }
+}
