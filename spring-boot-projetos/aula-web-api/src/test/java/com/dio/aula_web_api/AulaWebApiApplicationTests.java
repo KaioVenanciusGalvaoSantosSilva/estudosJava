ed05161ddb8c403286585e33ac75ea06ce98acec
Kaio Venâncius Galvão Santos Silva <kaioifsp@hotmail.com>
2024-08-17 00:30:31 -0300

    WEB APPLICATION API - REST API @RestController, @GetMapping

---
+++ spring-boot-projetos/aula-web-api/src/test/java/com/dio/aula_web_api/AulaWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.aula_web_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
