1c736ea6136f32b6492df311f8b618ef41158131
KaioVenanciusGalvaoSantosSilva <kaioifsp@hotmail.com>
2024-11-06 20:23:37 -0300

    Bootcamp e Dev

---
+++ desafio-dio-poo/src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,65 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+
+    private String nome;
+
+    //Polimorfismo para evitar acoplamento (SET sendo diferente de LinkedHashSet)
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>(); //Ordenado, criação e busca eficiente + elemetos únicos
+    private Set<Conteudo> conteudosConcluidos= new LinkedHashSet<>(); //Ordenado, criação e busca eficiente + elemetos únicos
+
+    public void inscreverBootcamp(Bootcamp bootcamp){
+
+    }
+    public void progredir(){
+
+    }
+
+    public double calcularXp(){
+
+        return 20;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    //Por usar LinkedHashSet precisa gerar o equals and hashcode
+    //Usado para comparar os elementos por meio do conteudo e da HASH
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
+}
